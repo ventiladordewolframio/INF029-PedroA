fb4cacdbc5a87dfa25f7aab5888a4cddb3c70b77
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2024-10-16 04:45:13 -0300

    interface basica + desisti por enquanto de fazer a saída dos caracteres especiais de portugues sairem corretamente

--- projeto_escola/ansi_escapes.c
+++ projeto_escola/ansi_escapes.c
@@ -3,47 +3,48 @@
 #endif
 
 #include <stdio.h>
+
 #include "ansi_escapes.h"
 
 #ifdef _WIN32
 // Some old MinGW/CYGWIN distributions don't define this:
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING  0x0004
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #endif
 
 static HANDLE stdoutHandle;
 static DWORD outModeInit;
 
 void setupConsole(void) {
-	DWORD outMode = 0;
-	stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD outMode = 0;
+    stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	if(stdoutHandle == INVALID_HANDLE_VALUE) {
-		exit(GetLastError());
-	}
-	
-	if(!GetConsoleMode(stdoutHandle, &outMode)) {
-		exit(GetLastError());
-	}
+    if (stdoutHandle == INVALID_HANDLE_VALUE) {
+        exit(GetLastError());
+    }
 
-	outModeInit = outMode;
-	
+    if (!GetConsoleMode(stdoutHandle, &outMode)) {
+        exit(GetLastError());
+    }
+
+    outModeInit = outMode;
+
     // Enable ANSI escape codes
-	outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-	if(!SetConsoleMode(stdoutHandle, outMode)) {
-		exit(GetLastError());
-	}	
+    if (!SetConsoleMode(stdoutHandle, outMode)) {
+        exit(GetLastError());
+    }
 }
 
 void restoreConsole(void) {
     // Reset colors
-    printf("\x1b[0m");	
-	
+    printf("\x1b[0m");
+
     // Reset console mode
-	if(!SetConsoleMode(stdoutHandle, outModeInit)) {
-		exit(GetLastError());
-	}
+    if (!SetConsoleMode(stdoutHandle, outModeInit)) {
+        exit(GetLastError());
+    }
 }
 #else
 void setupConsole(void) {}
--- projeto_escola/interface.c
+++ projeto_escola/interface.c
@@ -1,4 +1,8 @@
+
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "ansi_escapes.h"
 
 // ANSI colors
@@ -11,21 +15,44 @@
 int interfaceCad();
 int interfaceRel();
 
-int main() {
+bool sair = 0;
+
+int main(void) {
     setupConsole();
 
-    puts("\x1b[0;36m _____      _             __                 _____              _       \x1b[0;0m");
-    puts("\x1b[0;36m|_   _|    | |           / _|               |  ___|            | |  v0.1\x1b[0;0m");
-    puts("\x1b[0;36m  | | _ __ | |_ ___ _ __| |_ __ _  ___ ___  | |__ ___  ___ ___ | | __ _ \x1b[0;0m");
-    puts("\x1b[0;36m  | || '_ \\| __/ _ \\ '__|  _/ _` |/ __/ _ \\ |  __/ __|/ __/ _ \\| |/ _` |\x1b[0;0m");
-    puts("\x1b[0;36m _| || | | | ||  __/ |  | || (_| | (_|  __/ | |__\\__ \\ (_| (_) | | (_| |\x1b[0;0m");
-    puts("\x1b[0;36m \\___/_| |_|\\__\\___|_|  |_| \\__,_|\\___\\___| \\____/___/\\___\\___/|_|\\__,_|\x1b[0;0m");
+    while (!sair) {
+        puts("\x1b[0;36m _____      _             __                 _____              _       \x1b[0;0m");
+        puts("\x1b[0;36m|_   _|    | |           / _|               |  ___|            | |  v0.2\x1b[0;0m");
+        puts("\x1b[0;36m  | | _ __ | |_ ___ _ __| |_ __ _  ___ ___  | |__ ___  ___ ___ | | __ _ \x1b[0;0m");
+        puts("\x1b[0;36m  | || '_ \\| __/ _ \\ '__|  _/ _` |/ __/ _ \\ |  __/ __|/ __/ _ \\| |/ _` |\x1b[0;0m");
+        puts("\x1b[0;36m _| || | | | ||  __/ |  | || (_| | (_|  __/ | |__\\__ \\ (_| (_) | | (_| |\x1b[0;0m");
+        puts("\x1b[0;36m \\___/_| |_|\\__\\___|_|  |_| \\__,_|\\___\\___| \\____/___/\\___\\___/|_|\\__,_|\x1b[0;0m");
+        puts("\x1b[0;36m_________________________________________________________________________\x1b[0;0m");
+        puts("\x1b[0;31m(s) sair\x1b[0;0m");
+        puts("\x1b[0;32m(c) acessar interface de cadastros\x1b[0;0m");
+        puts("\x1b[0;32m(r) acessar interface de relatorios\x1b[0;0m");
+        printf("\x1b[0;36mEscolha:\x1b[0;0m ");
+        char operation;
+        scanf(" %c", &operation);
 
-    puts("\x1b[0;36m_________________________________________________________________________\x1b[0;0m");
+        switch (operation) {
+            case 's':// sai da aplicação
+                return 0;
+                break;
 
-    puts("\x1b[0;31m(s) - sair\x1b[0;0m");
-    puts("\x1b[0;32m(c) - acessar interface de cadastros\x1b[0;0m");
-    puts("\x1b[0;32m(r) - acessar interface de relatorios\x1b[0;0m");
+            case 'c':// cadastro
+                interfaceCad();
+                break;
+
+            case 'r':// relatorio
+                interfaceRel();
+                break;
+
+            default:// erro na operacao escolhida
+                printf("\n\x1b[0;31m[ERRO]: Escolha \"%c\" nao existe\n", operation);
+                break;
+        }
+    }
 
     restoreConsole();
     return 0;
