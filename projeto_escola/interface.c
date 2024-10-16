ead5e7ca5913214802d4bd3ebc6c51462ae09a26
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2024-10-16 06:40:20 -0300

    ta tarde (cedo), alguns bugs no processo de pegar os dados escritos no stdin e salva-los corretamente nas variaveis de struct criadas

--- projeto_escola/interface.c
+++ projeto_escola/interface.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "ansi_escapes.h"
 
@@ -12,17 +13,57 @@
 // |   cyan \x1b[0;36m
 // |  blank \x1b[0;0m
 
+struct data {
+    unsigned long long int timestamp;
+    int ano;
+    int mes;
+    int dia;
+};
+
+struct aluno {
+    int matricula;
+    char nome[256];
+    char sexo;
+    struct data nascimento;
+    int cpf;
+};
+
+struct professor {
+    int matricula;
+    char nome[256];
+    char sexo;
+    struct data nascimento;
+    int cpf;
+};
+
+struct disciplina {
+    char nome[128];
+    int id;
+    int semestre;
+    char professor[128];
+};
+
+#define num_alunos 20
+#define num_professores 10
+#define num_disciplinas 5
+
+struct aluno alunos[num_alunos];
+struct professor professores[num_professores];
+struct disciplina disciplinas[num_disciplinas];
+
+void writeDatabase();
+void readDatabase();
 int interfaceCad();
 int interfaceRel();
 
-bool sair = 0;
-
 int main(void) {
+    bool sair = 0;
+
     setupConsole();
 
     while (!sair) {
         puts("\x1b[0;36m _____      _             __                 _____              _       \x1b[0;0m");
-        puts("\x1b[0;36m|_   _|    | |           / _|               |  ___|            | |  v0.2\x1b[0;0m");
+        puts("\x1b[0;36m|_   _|    | |           / _|               |  ___|            | |  v0.3\x1b[0;0m");
         puts("\x1b[0;36m  | | _ __ | |_ ___ _ __| |_ __ _  ___ ___  | |__ ___  ___ ___ | | __ _ \x1b[0;0m");
         puts("\x1b[0;36m  | || '_ \\| __/ _ \\ '__|  _/ _` |/ __/ _ \\ |  __/ __|/ __/ _ \\| |/ _` |\x1b[0;0m");
         puts("\x1b[0;36m _| || | | | ||  __/ |  | || (_| | (_|  __/ | |__\\__ \\ (_| (_) | | (_| |\x1b[0;0m");
@@ -31,25 +72,25 @@ int main(void) {
         puts("\x1b[0;31m(s) sair\x1b[0;0m");
         puts("\x1b[0;32m(c) acessar interface de cadastros\x1b[0;0m");
         puts("\x1b[0;32m(r) acessar interface de relatorios\x1b[0;0m");
-        printf("\x1b[0;36mEscolha:\x1b[0;0m ");
-        char operation;
-        scanf(" %c", &operation);
+        printf("\x1b[0;36m[INFO]: Escolha:\x1b[0;0m ");
+        char op;
+        scanf(" %c", &op);
 
-        switch (operation) {
-            case 's':// sai da aplicação
+        switch (op) {
+            case 's':  // sai da aplicação
                 return 0;
                 break;
 
-            case 'c':// cadastro
+            case 'c':  // cadastro
                 interfaceCad();
                 break;
 
-            case 'r':// relatorio
+            case 'r':  // relatorio
                 interfaceRel();
                 break;
 
-            default:// erro na operacao escolhida
-                printf("\n\x1b[0;31m[ERRO]: Escolha \"%c\" nao existe\n", operation);
+            default:  // erro na operacao escolhida
+                printf("\x1b[0;31m[ERRO]: Escolha \"%c\" nao existe\n", op);
                 break;
         }
     }
@@ -58,7 +99,97 @@ int main(void) {
     return 0;
 }
 
+void writeDatabase() {
+}
+void readDatabase() {
+}
 int interfaceCad() {
+    bool sair_cad = 0;
+    while (!sair_cad) {
+        puts("\x1b[0;36m _____           _           _             \x1b[0;0m");
+        puts("\x1b[0;36m/  __ \\         | |         | |            \x1b[0;0m");
+        puts("\x1b[0;36m| /  \\/ __ _  __| | __ _ ___| |_ _ __ ___  \x1b[0;0m");
+        puts("\x1b[0;36m| |    / _` |/ _` |/ _` / __| __| '__/ _ \\ \x1b[0;0m");
+        puts("\x1b[0;36m| \\__/\\ (_| | (_| | (_| \\__ \\ |_| | | (_) |\x1b[0;0m");
+        puts("\x1b[0;36m \\____/\\__,_|\\__,_|\\__,_|___/\\__|_|  \\___/ \x1b[0;0m");
+        puts("\x1b[0;36m____________________________________________\x1b[0;0m");
+        puts("\x1b[0;31m(s) sair\x1b[0;0m");
+        puts("\x1b[0;32m(a) aluno\x1b[0;0m");
+        puts("\x1b[0;32m(p) professor\x1b[0;0m");
+        puts("\x1b[0;32m(d) disciplina\x1b[0;0m");
+        printf("\x1b[0;36m[INFO]: Escolha:\x1b[0;0m ");
+        char op;
+        scanf(" %c", &op);
+
+        switch (op) {
+            case 's':  // sai da aplicação
+                return 0;
+                break;
+
+            case 'a':  // alunos
+            printf("\n");
+                for (int i = 0; i < num_alunos; i++) {
+                    printf("\x1b[0;36m[INFO]: Insira o Nome do aluno matricula \"%d\":\x1b[0;0m\n ", i);
+                    char nome[256];
+                    fgets(nome, 256, stdin);
+                    strcpy(alunos[i].nome, nome);
+                }
+                for (int i = 0; i < num_alunos; i++) {
+                    printf("\x1b[0;36m[INFO]: aluno \"%d\" nome:\x1b[0;0m ", i);
+                    printf("\"");
+                    for (int c = 0; c < (sizeof(&alunos[i].nome[c])/sizeof(char)); c++) {
+                        printf("%c", alunos[i].nome[c]);
+                    }
+                    printf("\"");
+                    printf("\n");
+                }
+                break;
+
+            case 'p':  // professores
+
+                break;
+
+            case 'd':  // disciplinas
+
+                break;
+
+            default:  // erro na operacao escolhida
+                printf("\x1b[0;31m[ERRO]: Escolha \"%c\" nao existe\n", op);
+                break;
+        }
+    }
 }
 int interfaceRel() {
+    bool sair_rel = 0;
+    while (!sair_rel) {
+        puts("\x1b[0;36m______     _       _             _           \x1b[0;0m");
+        puts("\x1b[0;36m| ___ \\   | |     | |           (_)          \x1b[0;0m");
+        puts("\x1b[0;36m| |_/ /___| | __ _| |_ ___  _ __ _  ___  ___ \x1b[0;0m");
+        puts("\x1b[0;36m|    // _ \\ |/ _` | __/ _ \\| '__| |/ _ \\/ __|\x1b[0;0m");
+        puts("\x1b[0;36m| |\\ \\  __/ | (_| | || (_) | |  | | (_) \\__ \\\x1b[0;0m");
+        puts("\x1b[0;36m\\_| \\_\\___|_|\\__,_|\\__\\___/|_|  |_|\\___/|___/\x1b[0;0m");
+        puts("\x1b[0;36m______________________________________________\x1b[0;0m");
+        puts("\x1b[0;31m(s) sair\x1b[0;0m");
+        printf("\x1b[0;36m[INFO]: Escolha:\x1b[0;0m ");
+        char op;
+        scanf(" %c", &op);
+
+        switch (op) {
+            case 's':  // sai da aplicação
+                return 0;
+                break;
+
+            case 'c':  // cadastro
+
+                break;
+
+            case 'r':  // relatorio
+
+                break;
+
+            default:  // erro na operacao escolhida
+                printf("\x1b[0;31m[ERRO]: Escolha \"%c\" nao existe\n", op);
+                break;
+        }
+    }
 }
