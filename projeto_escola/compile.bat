4f2659226924c7c8535881565e4c4401c5005923
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2024-10-19 02:06:09 -0300

    mudança minima nesses dois arquivos em questão de formatação, pois nem sempre que termina o arquivo é criado mas sempre a compilação é terminada

--- projeto_escola/compile.bat
+++ projeto_escola/compile.bat
@@ -1,7 +1,8 @@
 @echo off
 
-echo [0;33mcompiling code ...[0;0m
+echo.
+echo [0;33mcompiling program.win.bin ...[0;0m
 
 gcc interface.c ansi_escapes.c -o program.win.exe
 
-echo [0;32mcompilation complete, program.win.exe created.[0;0m
+echo [0;32mcompilation complete.[0;0m
--- projeto_escola/compile.sh
+++ projeto_escola/compile.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/bash
 
-echo -e "\033[0;33m compiling code ... \033[0;0m"
+echo ""
+echo -e "\033[0;33m compiling \"program.lin.bin\" ...\033[0;0m"
 
 gcc interface.c ansi_escapes.c -o program.lin.bin
 
-echo -e "\033[0;32m compilation complete, program.lin.bin created. \033[0;0m"
+echo -e "\033[0;32m compilation complete. \033[0;0m"
+echo ""
