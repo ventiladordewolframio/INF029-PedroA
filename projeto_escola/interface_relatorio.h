84472167a05f58a5a6fce1c18a93d534c0d2947f
Deivisson <deivissonviniciuss@gmail.com>
2024-10-27 01:03:05 -0300

    Update interface_relatorio.h

--- projeto_escola/interface_relatorio.h
+++ projeto_escola/interface_relatorio.h
@@ -1,7 +1,13 @@
 #ifndef INTERFACE_RELATORIO_H
 #define INTERFACE_RELATORIO_H
 
+#include "interface_main.h"
+
 int interfaceRel();
 void listarAlunoPorSexo(struct aluno alunos[], int qtd_aluno);
+void listarProfessorPorSexo(struct professor professores[], int qtd_professor);
+int listarAniversariante(struct aluno alunos[], struct professor professores[], int qtd_aluno, int qtd_professor);
+void listarAlunoComMenosDeTresDisciplinas(struct aluno alunos[], int qtd_aluno);
+void listarDisciplinasQueExtrapolam40Vagas(struct disciplina disciplinas[], int qtd_disciplina);
 
 #endif
