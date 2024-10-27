2f69099c05e6b66a9958e95c46fd89b6f7ce21d6
Deivisson <deivissonviniciuss@gmail.com>
2024-10-27 01:02:17 -0300

    Update interface_relatorio.c

--- projeto_escola/interface_relatorio.c
+++ projeto_escola/interface_relatorio.c
@@ -96,7 +96,7 @@ int interfaceRel() {
                 break;
 
             case 7:  // listar professores por sexo
-                
+                listarProfessorPorSexo(professores, qtd_professor);
                 break;
 
             case 8:  // listar professores por nome
@@ -108,7 +108,7 @@ int interfaceRel() {
                 break;
 
             case 10:  // listar aniversariantes do mês
-                //! inserir chamada de funcao aqui
+                listarAniversariante(alunos, professores, qtd_aluno, qtd_professor);
                 break;
 
             case 11:  // listar pessoas (buscar)
@@ -116,11 +116,11 @@ int interfaceRel() {
                 break;
 
             case 12:  // listar alunos matriculados em menos de 3 disciplinas
-                //! inserir chamada de funcao aqui
+                listarAlunoComMenosDeTresDisciplinas(alunos, qtd_aluno);
                 break;
 
             case 13:  // listar disciplinas que extrapolam 40 vagas
-                //! inserir chamada de funcao aqui
+                listarDisciplinasQueExtrapolam40Vagas(disciplinas, qtd_disciplina);
                 break;
 
             default:  // erro na operacao escolhida
@@ -134,7 +134,6 @@ void listarAlunoPorSexo(struct aluno alunos[], int qtd_aluno) {
     if(qtd_aluno == 0){
         printf("Sem alunos cadastrados\n");
     }else {
-        getchar();
         printf("Digite o sexo a ser listado (M ou F)");
         char sexo;
         scanf(" %c", &sexo);
@@ -163,5 +162,123 @@ void listarAlunoPorSexo(struct aluno alunos[], int qtd_aluno) {
             
         } else {
             printf("Sexo inválido! Digite M ou F\n");
+        }
     }
+}
+//não entendi ainda como usar ponteiros então vou ter que repetir a função pra professor :(
+//ah e tentei substituir as entradas pelos inputs, mas nao deu certo (os ponteiros!!!)
+
+void listarProfessorPorSexo(struct professor professores[], int qtd_professor) {
+    if(qtd_professor == 0){
+        printf("Sem professores cadastrados\n");
+    }else {
+        printf("Digite o sexo a ser listado (M ou F)");
+        char sexo;
+        scanf(" %c", &sexo);
+        sexo = toupper(sexo);
+
+        if(sexo == 'M'){
+            printf("\n--LISTA DE PROF. DO SEXO MASCULINO--\n\n");
+            for(int i=0; i<qtd_professor; i++){
+                if (alunos[i].sexo == 'M') {
+                    printf("Matrícula: %d\n", professores[i].matricula);
+                    printf("Nome: %s\n", professores[i].nome);
+                    printf("CPF: %s\n", professores[i].cpf);
+                    printf("Data de nascimento: %d/%d/%d\n", professores[i].nascimento.dia, professores[i].nascimento.mes, professores[i].nascimento.ano);
+                }
+            }
+        } else if(sexo == 'F'){
+            printf("\n--LISTA DE PROF. DO SEXO FEMININO--\n\n");
+            for(int i=0; i<qtd_professor; i++){
+                if (professores[i].sexo == 'F') {
+                    printf("Matrícula: %d\n", professores[i].matricula);
+                    printf("Nome: %s\n", professores[i].nome);
+                    printf("CPF: %s\n", professores[i].cpf);
+                    printf("Data de nascimento: %d/%d/%d\n", professores[i].nascimento.dia, professores[i].nascimento.mes, professores[i].nascimento.ano);
+                }
+            }
+
+        } else {
+            printf("Sexo inválido! Digite M ou F\n");
+        }
+    }
+}
+
+int listarAniversariante(struct aluno alunos[], struct professor professores[], int qtd_aluno, int qtd_professor){
+    if(qtd_aluno == 0 && qtd_professor == 0){
+        printf("Não exitem alunos ou professores cadastradoss\n");
+    } else{
+        printf("Insira o mês (em algarismo) para buscar aniversariantes\n");
+        int mes;
+        scanf("%d", &mes);
+    
+        if(mes < 1 || mes > 12){
+            printf("Mês inválido! Digite um mês entre 1 e 12\n");
+        } else {
+            int qtdAniversariante = 0;
+            if(qtd_aluno > 0){
+                printf("\n----------ALUNOS ANIVERSARIANTES----------\n");
+                for(int i=0; i<qtd_aluno; i++){
+                    if(alunos[i].ativo == true && alunos[i].nascimento.mes == mes){
+                        printf("\nNome: %s\n", alunos[i].nome);
+                        printf("Mátricula: %d\n", alunos[i].matricula);
+                        qtdAniversariante++;
+                    }
+                }
+            }
+            if(qtd_professor > 0){
+                printf("\n----------PROFESSORES ANIVERSARIANTES----------\n");
+                for(int i=0; i<qtd_professor; i++){
+                    if(professores[i].ativo == true && professores[i].nascimento.mes == mes){
+                        printf("\nNome: %s\n", professores[i].nome);
+                        printf("Mátricula: %d\n", professores[i].matricula);
+                        qtdAniversariante++;
+                    }
+                }
+            }
+            if(qtdAniversariante == 0)
+                printf("Não há aniversariantes nesse mês\n");
+            else 
+                printf("\nListagem concluída!\n");
+        }
+    }
+}
+
+void listarAlunoComMenosDeTresDisciplinas(struct aluno alunos[], int qtd_aluno) {
+    int listados = 0;
+    int j = 0;
+    while(j < qtd_aluno){
+        int qtdDisciplinas = 0;
+        for (int i = 0; i < MAX_num_disciplinas; i++) {
+            if (alunos[j].disciplinas[i]) {
+                qtdDisciplinas++;
+            }
+        }
+        if (qtdDisciplinas < 3){
+            printf("Nome: %s\n", alunos[j].nome);
+            printf("Matrícula: %d\n", alunos[j].matricula);
+            listados++;
+        }
+        j++;
+    }
+    if(listados == 0){
+        printf("Não há alunos com menos de 3 disciplinas\n");
+    }
+}
+
+void listarDisciplinasQueExtrapolam40Vagas(struct disciplina disciplinas[], int qtd_disciplina){
+    if(qtd_disciplina == 0){
+        printf("Não há disciplinas cadastradas\n");
+    } else {
+        int qtdExtrapoladas = 0;
+        for(int i=0; i<qtd_disciplina; i++){
+            if(disciplinas[i].vaga_preenchida > 40)
+                printf("Disciplina: %s\n", disciplinas[i].nome);
+                printf("Código: %d\n", disciplinas[i].id);
+                qtdExtrapoladas++;
+        }
+        if(qtdExtrapoladas == 0)
+            printf("Não há disciplinas com mais de 40 vagas\n");
+    }
+
 }
